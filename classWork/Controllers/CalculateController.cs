0bb2534cc09b1cc681cb8e1c47ba0056adbbba5b
Сергей Назаров <nazaroff.serezha2014@gmail.com>
2022-02-12 13:53:48 +0300

    ыфв

---
+++ classWork/Controllers/CalculateController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classWork.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculateController : ControllerBase
+    {
+        [HttpGet]
+        [Route("sum")]
+        public async Task<string> Sum(int a, int b)
+        {
+            var sum = a + b;
+            return sum.ToString();
+        }
+        [HttpGet]
+        [Route("minus")]
+        public async Task<string> Minus(int a, int b)
+        {
+            var sum = a - b;
+            return sum.ToString();
+        }
+        [HttpGet]
+        [Route("delenie")]
+        public async Task<string> Delenie(int a, int b)
+        {
+            var sum = a / b;
+            return sum.ToString();
+        }
+        [HttpGet]
+        [Route("umnozh")]
+        public async Task<string> Umnozh(int a, int b)
+        {
+            var sum = a * b;
+            return sum.ToString();
+        }
+    }
+}
